d99de91d5fd0135fa647108b9fa0f5adbe94b39c
Rh96 <radehrgo96@gmail.com>
2026-05-20 16:33:46 +0200

    baseURL change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
   app: {
-    baseURL: '/portfolio-site/',
+    baseURL: '/',
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1.0',
